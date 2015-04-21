4923eee8b98adf66250e53989ef261c9b68726fc
luckavaz95 <luckavaz95@gmail.com>
2015-04-21 14:51:46 -0300

    TP2 "Número Par"

---
+++ TP2.cpp
@@ -0,0 +1,18 @@
+/* Trabajo Práctico 2 "Número Par"
+* Lucas Vázquez
+* 21/04/2015
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+		int A;
+		cout<<"Ingrese un numero entero \n";
+		cin>>A;
+		if (A%2==0)
+			cout<<"El numero ingresado es Par";
+		else
+			cout<<"El numero ingresado no es Par";
+}
